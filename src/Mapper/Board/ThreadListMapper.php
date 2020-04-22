8e8496ec7f4c9935eb9c0553415370616c35415d
StereoFlo <stereoflo@users.noreply.github.com>
2020-04-22 15:27:04 +0300

    sort

--- src/Mapper/Board/ThreadListMapper.php
+++ src/Mapper/Board/ThreadListMapper.php
@@ -2,10 +2,18 @@
 
 namespace App\Mapper\Board;
 
+use function array_map;
+
 class ThreadListMapper
 {
     public static function map(array $threads): array
     {
-        return \array_map([ThreadMapper::class, 'map'], $threads);
+        $data = array_map([ThreadMapper::class, 'map'], $threads);
+
+        $scores = array_column($data, 'score');
+
+        array_multisort($scores, SORT_DESC, $data);
+
+        return $data;
     }
 }
